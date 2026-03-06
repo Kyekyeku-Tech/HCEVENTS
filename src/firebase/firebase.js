c255a5d02eae7be25f6f308d52c3e9cb41851196
Kyekyeku-Tech <safooppong121@gmail.com>
2026-03-06 01:02:45 +0000

    secure credentials

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6TJLvN8AKSAWKGPZJr8MSsJD5GdP62gA",
-  authDomain: "hcevent-713d6.firebaseapp.com",
-  projectId: "hcevent-713d6",
-  storageBucket: "hcevent-713d6.firebasestorage.app",
-  messagingSenderId: "366369208649",
-  appId: "1:366369208649:web:e60c3b7935f91bc644c298",
-  measurementId: "G-4C76C0VMT2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
